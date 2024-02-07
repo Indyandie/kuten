acef069b3a98f6f62c5524e4adbc0be3fc5dfc75
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-02-06 23:11:29 -0600

    nvchad: overrides - fix modes

--- nvim/nvchad/chadrc.lua
+++ nvim/nvchad/chadrc.lua
@@ -5,28 +5,45 @@ local M = {}
 local highlights = require("custom.highlights")
 
 local modes = {
-	["n"] = { "norm", "◆", "St_NormalMode" },
-	["niI"] = { "norm i", "◆", "St_NormalMode" },
-	["niR"] = { "norm r", "◆", "St_NormalMode" },
-	["niV"] = { "norm v", "◆", "St_NormalMode" },
-	["no"] = { "N-PENDING", "◆", "St_NormalMode" },
-	["i"] = { "in", "●", "St_InsertMode" },
-	["ic"] = { "in (completion)", "●", "St_InsertMode" },
-	["ix"] = { "in completion", "●", "St_InsertMode" },
+  -- See https://github.com/NvChad/ui/blob/v2.0/lua/nvchad/statusline/default.lua
+
+  -- Normal
+	["n"] = { "nor", "●", "St_NormalMode" },
+	["niI"] = { "nor i", "●", "St_NormalMode" },
+	["niR"] = { "nor r", "●", "St_NormalMode" },
+	["niV"] = { "nor v", "●", "St_NormalMode" },
+	["no"] = { "N-PENDING", "●", "St_NormalMode" },
+
+  -- Insert
+	["i"] = { "ins", "◉", "St_InsertMode" },
+	["ic"] = { "ins (completion)", "◉", "St_InsertMode" },
+	["ix"] = { "ins completion", "◉", "St_InsertMode" },
+
+  -- Terminal
 	["t"] = { "term", "❯", "St_TerminalMode" },
 	["nt"] = { "nterm", "❯", "St_NTerminalMode" },
-	["v"] = { "vis", "▼", "St_VisualMode" },
-	["V"] = { "vis-ln", "▼", "St_VisualMode" },
-	["Vs"] = { "v-ln(Ctrl O)", "▼", "St_VisualMode" },
-	[""] = { "vlk", "▼", "St_VisualMode" },
+
+  -- Visual
+	["v"] = { "vis", "◍", "St_VisualMode" },
+	["V"] = { "vis-ln", "◍", "St_VisualMode" },
+	["Vs"] = { "v-ln(Ctrl O)", "◍", "St_VisualMode" },
+	[""] = { "vlk", "◍", "St_VisualMode" },
+
+  -- Replace
 	["R"] = { "rep", "◌", "St_ReplaceMode" },
 	["Rv"] = { "v-rep", "◌", "St_ReplaceMode" },
+
+  -- Select
 	["s"] = { "sel", "◎", "St_SelectMode" },
 	["S"] = { "sel-ln", "◎", "St_SelectMode" },
 	[""] = { "sel-blk", "◎", "St_SelectMode" },
+
+  -- Command
 	["c"] = { "cmd", "⨀", "St_CommandMode" },
 	["cv"] = { "cmd", "⨀", "St_CommandMode" },
 	["ce"] = { "cmd", "⨀", "St_CommandMode" },
+
+  -- Confirm
 	["r"] = { "prompt", "┅", "St_ConfirmMode" },
 	["rm"] = { "more", "+", "St_ConfirmMode" },
 	["r?"] = { "confirm", "✓", "St_ConfirmMode" },
@@ -43,6 +60,20 @@ M.ui = {
 	statusline = {
 		theme = "minimal",
 		separator_style = "block",
+
+    overriden_modules = function(modules)
+      modules[1] = (function()
+        local m = vim.api.nvim_get_mode().mode
+        return "%#"
+						.. modes[m][3] .. "#" .. " " .. modes[m][2] .. " " .. modes[m][1] .. " " .. "%#" .. modes[m][3] .. "Sep" .. "#"
+      end)()
+
+      table.insert(
+        modules,
+        2,
+        " "
+      )
+    end,
 	},
 }
 
