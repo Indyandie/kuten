966d0f03f3ca53db14fe9b0627d046334d3fbc9a
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-06-01 17:15:02 -0500

    feat(eza): add user `uu` style

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -160,7 +160,7 @@ alias man="batman"
 
 export EZA_ICONS_AUTO=true
 export LS_COLORS="di=35;4;1;3:ex=31;1;3:fi=37;3:ln=94;1;3;4"
-export EZA_COLORS="lp=35;3;4:*.md=92;3:ur=32;3:uw=33:ux=31;1;3:ue=31;3:gr=32;3:gw=33:gx=31;1;3:tr=32;3:tw=33:tx=31;1;3:da=90:nb=37;3;2:nk=37;3:nm=96;1:ng=31;1;3:do=32;3:im=96;3:vi=36;1"
+export EZA_COLORS="lp=35;3;4:*.md=92;3:uu=33;1;3:ur=32;3:uw=33:ux=31;1;3:ue=31;3:gr=32;3:gw=33:gx=31;1;3:tr=32;3:tw=33:tx=31;1;3:da=90:nb=37;3;2:nk=37;3:nm=96;1:ng=31;1;3:do=32;3:im=96;3:vi=36;1"
 
 alias lz="eza"
 alias la="eza -la"
